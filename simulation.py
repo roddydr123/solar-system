2ebc164d5a3c0167bdcf6735173b010443175d7c
roddydr123 <davidroddy321@gmail.com>
2023-06-02 16:37:32 +0100

    satellite incorporated

--- simulation.py
+++ simulation.py
@@ -14,134 +14,21 @@ added a function to order self.bodies by mass
 added a function to print the time when the satellite comes within a given radius of mars
 modified mars' (and satellite's) position to make the satellite come close to it
 gives initial velocity of satellite relative to Earth)
-
-No satellite (code is there, just nulled), energy graphs and periods are printed
 """
 
-import math
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from utils import Body
 
 G = 6.673E-11
-
-
-class Body(object):
-    
-    
-    def __init__(self,position,velocity,radius,colour,mass,name,calcinit,period):
-        
-        # Constructor method to create instance variables for each body.
-        
-        self.position = position
-        self.velocity = velocity
-        self.radius = radius
-        self.colour = colour
-        self.mass = mass
-        self.name = name
-        self.calcinit = calcinit
-        self.period = period
-        self.ahist = [0,0,0,0]
-        
-    def v_init(self,bodies):
-        
-        # Using v = sqrt(GM/r) finds the orbital velocity.  This
-        # is then split into components in the second code block so that the
-        # direction of motion is always perpendicular to the orbital radius.
-        
-        r = (self.position[0])**2 + (self.position[1])**2
-        r = math.sqrt(r)
-        v = math.sqrt(G*bodies[0].mass/r)
-        
-        if self.position[0] == 0:
-            self.position[0] = 1    
-        d = (self.position[1]/self.position[0])
-        self.velocity[1] = math.sqrt((v**2) / (1 + d**2))
-        self.velocity[0] = - d * self.velocity[1]
-        
-    def calc_a(self,bodies,body_index):
-        
-        # This function is called on each body in turn.  The acceleration of
-        # this body due to each other body is then calculated and added to
-        # a total. This is returned and passed to calc_v.
-        
-        acceleration = [0,0]
-        target = bodies[body_index]
-        for index, otherbod in enumerate(bodies):
-            if index != body_index:
-                r = (target.position[0]-otherbod.position[0])**2 + (target.position[1]-otherbod.position[1])**2
-                r = math.sqrt(r)
-                force = G * otherbod.mass / r**3
-                acceleration[0] += force * (otherbod.position[0] - target.position[0])
-                acceleration[1] += force * (otherbod.position[1] - target.position[1])
-        return acceleration
-
-    def calc_v(self,bodies,dt):
-        
-        # Calls calc_a on each body in turn and finds the new velocity of each
-        # body based on its output.  Stores the previous acceleration values
-        # in body.ahist so they can be used by Beeman algo.
-        
-        for a,body in enumerate(bodies):
-            acceleration = self.calc_a(bodies,a) 
-            body.ahist.append(acceleration[0])
-            body.ahist.append(acceleration[1])
-            body.velocity[0] += 1/6 * ((2 * body.ahist[4]) + (5 * body.ahist[2]) - (body.ahist[0])) * dt   
-            body.velocity[1] += 1/6 * ((2 * body.ahist[5]) + (5 * body.ahist[3]) - (body.ahist[1])) * dt
-    def calc_p(self,bodies,dt):
-        
-        # For every body in the simulation, its position is updated using the
-        # Beeman algo and previous acceleration values stored in ahist (which
-        # is a six-element list).  After the calculations are done, the first
-        # two elements of the list are deleted corresponding with the oldest
-        # values of acceleration.
-        
-        for num,body in enumerate(bodies):
-            body.position[0] += (body.velocity[0] * dt) + 1/6 * (4 * body.ahist[2] - body.ahist[0]) * (dt)**2
-            body.position[1] += (body.velocity[1] * dt) + 1/6 * (4 * body.ahist[3] - body.ahist[1]) * (dt)**2
-            del body.ahist[0:2]
-                
-    def step(self,bodies,dt):
-        
-        # Called by animate to perform a single step of size dt.
-        
-        self.calc_v(bodies,dt)
-        self.calc_p(bodies,dt)
-        
-    def kinetic(self,bodies):
-        
-        # Finds the total kinetic energy of the system by
-        # adding up the contribution from each body using 1/2 mv**2.
-        
-        K = 0
-        for a,body in enumerate(bodies):
-            body = bodies[a]
-            vsq = body.velocity[0]**2 + body.velocity[1]**2
-            K += 0.5 * body.mass * vsq
-        return K
-    
-    def potential(self,bodies):
-        
-        # Goes through each pair of bodies and finds the potential between
-        # them.  Adds them up and returns to animate function to be added
-        # to kinetic.
-        
-        P = 0
-        for a,body in enumerate(bodies):
-            body = bodies[a]
-            for num,target in enumerate(bodies):
-                target = bodies[num]
-                if a != num:
-                    r = math.sqrt((body.position[0] - target.position[0])**2 + (body.position[1] - target.position[1])**2)
-                    U = G * body.mass * target.mass / r
-                    P += -1/4 * U
-        return P
     
     
 class Simulation(object):
 
     
-    def __init__(self):
+    def __init__(self, satellite):
         
         # Add new bodies here, remember to include them in self.bodies.  The
         # third block calculates the initial velocity of each body 
@@ -166,6 +53,7 @@ class Simulation(object):
         self.nrghist = []
         self.time = []
         self.reached = False
+        self.satellite = satellite
         
         for a,body in enumerate(self.bodies):
             body = self.bodies[a]
@@ -174,9 +62,11 @@ class Simulation(object):
                 if body.name == 'Mars':         # makes sure Mars is orbiting in the correct direction
                     body.velocity[0] = -1 *body.velocity[0]
                     body.velocity[1] = -1 *body.velocity[1]
-        #self.addsat()
+
+        if self.satellite is True:
+            self.addsat()
         
-    def init(self):
+    def animation_init(self):
         
         # This function is called once before animate to put the patches in place.
         
@@ -201,8 +91,8 @@ class Simulation(object):
                 satellite.velocity[0] = body.velocity[0] + sx
                 satellite.velocity[1] = body.velocity[1] + sy
         
-        vinit = math.sqrt(sx**2 + sy**2)
-        print("inital speed is " + str(vinit) + " ms^-1")
+        vinit = np.sqrt(sx**2 + sy**2)
+        print("inital satellite speed is " + str(vinit) + " m/s")
         self.bodies.append(satellite)
     
     def timesat(self,i):
@@ -217,7 +107,7 @@ class Simulation(object):
                 q = body.position
             if body.name == 'Mars':
                 n = body.position
-        s = math.sqrt((q[0] - n[0])**2 + (q[1] - n[1])**2)
+        s = np.sqrt((q[0] - n[0])**2 + (q[1] - n[1])**2)
         
         if s <= 7E9 and self.reached == False:
             w = round((self.dt * i / 31536000*12), 2)
@@ -232,13 +122,13 @@ class Simulation(object):
         for a,body in enumerate(self.bodies):
             body = self.bodies[a]
             if body.period == True and i % 200 == 0:
-                r = math.sqrt((body.position[0] - self.bodies[0].position[0])**2 + (body.position[1] - self.bodies[0].position[1])**2)
-                f = math.sqrt(r**3 / (G * self.bodies[0].mass))
-                t = 2 * math.pi * f
+                r = np.sqrt((body.position[0] - self.bodies[0].position[0])**2 + (body.position[1] - self.bodies[0].position[1])**2)
+                f = np.sqrt(r**3 / (G * self.bodies[0].mass))
+                t = 2 * np.pi * f
                 h = t / 31536000        # conversion to years
                 print(str(body.name) + " : " + str(round(h,2)) + " years")
     
-    def animate(self,i):
+    def animate(self, i, satellite):
         
         # self.dt sets the speed of the animation.  Then p.step() updates the
         # positions of all bodies.  The third block calls the function to
@@ -250,8 +140,11 @@ class Simulation(object):
         
         p = self.bodies[0]
         p.step(self.bodies,self.dt)
-        self.eperiod(i)
-        #self.timesat(i)
+
+        if satellite is True:
+            self.timesat(i)
+        else:
+            self.eperiod(i)
         
         if i % 10 == 0:
             K = p.kinetic(self.bodies)
@@ -269,7 +162,7 @@ class Simulation(object):
             self.patches[a].center = (body.position[0], body.position[1])
         return self.patches
         
-    def sim(self):
+    def sim(self, satellite):
         
         # This func adds patches in the locations of the bodies and
         # sets the size of the axes.  Then the ani variable repeatedly calls
@@ -288,7 +181,7 @@ class Simulation(object):
         ax1.set_xlim(-2.5e11,2.5e11)
         ax1.set_ylim(-2.5e11,2.5e11)
         
-        anim = FuncAnimation(fig1, self.animate, init_func = self.init, frames = 10000, repeat = False, interval = 20, blit = True)
+        anim = FuncAnimation(fig1, self.animate, fargs=[satellite], init_func = self.animation_init, frames = 10000, repeat = False, interval = 20, blit = True)
         plt.show()
         
     def nrggraph(self):
@@ -306,10 +199,17 @@ class Simulation(object):
         plt.show()
 
 def main():
+
+    satellite = False
     
+    try:
+        if sys.argv[1] == "sat":
+            satellite = True
+    except IndexError:
+        pass
     
-    test = Simulation()
-    test.sim()
+    test = Simulation(satellite)
+    test.sim(satellite)
 
 
 main()
